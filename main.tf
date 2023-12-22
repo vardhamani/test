9648e7477e96a9ff80afcdb9aafd3f61b90daeb4
vardhamani <121101210+vardhamani@users.noreply.github.com>
2023-12-22 20:45:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+# Configure the AWS provider with your AWS access and secret keys
+provider "aws" {
+  region = "us-east-1"  # Change to your desired AWS region
+}
+
+# Define the EC2 instance resource
+resource "aws_instance" "example" {
+  ami           = "ami-079db87dc4c10ac91" 
+  instance_type = "t2.micro"
+  tags = {
+    Name = "MyEC2Instance"  # Replace with your desired instance name
+  }
+}
